2059713351287dbeda45cb1982580671f980b6d6
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2022-02-08 11:27:44 +0530

    Added reference to indigo pink material theme - FAILED

--- scope/angmatenv/angmatenv.extension.ts
+++ scope/angmatenv/angmatenv.extension.ts
@@ -2,16 +2,21 @@ import { EnvsMain, EnvsAspect } from '@teambit/envs'
 import { AngularV13Aspect, AngularV13Main } from '@teambit/angular-v13'
 
 export class AngmatenvExtension {
-  constructor(private angular: AngularV13Main) {}
+  constructor(private angular: AngularV13Main) { }
 
   static dependencies: any = [EnvsAspect, AngularV13Aspect]
 
   static async provider([envs, angular]: [EnvsMain, AngularV13Main]) {
     // Use any of the "angular.override..." transformers, for example:	
-	const compilerOptions = await angular.overrideCompilerOptions({
-      // fullTemplateTypeCheck: false
+    // const compilerOptions = await angular.overrideCompilerOptions({
+    //   // fullTemplateTypeCheck: false
+    // });
+    const angularOptions = angular.overrideAngularOptions({
+      styles: [
+        require.resolve('@angular/material/prebuilt-themes/indigo-pink.css')
+      ]
     });
-    const AngmatenvEnv = angular.compose([compilerOptions])
+    const AngmatenvEnv = angular.compose([angularOptions])
     envs.registerEnv(AngmatenvEnv)
 
     return new AngmatenvExtension(angular)
